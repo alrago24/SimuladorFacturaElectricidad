df38c480733f7a40bd1174226473950c8f79d3dd
alrago24 <alinson_04@hotmail.com>
2025-08-07 21:04:40 -0500

    Se crea última función donde se brinda el detalle de la factura

--- script.js
+++ script.js
@@ -26,4 +26,8 @@ let totalAPagar = () => {
     return subTotalTarifa() + valorIvaAplicado() + cargoFijo();
 }
 
+function detalleFactura() {
+    console.log(`Detalle a pagar de su factura\n Consumo mensual: ${consumoMensual()} KVh \n Tarifa por Kilovatio: ${tarifaPorKilovatio()} COP\n Cargo fijo: ${cargoFijo()} COP\n Subtotal a pagar: ${subTotalTarifa()} COP\n Valor IVA: ${valorIvaAplicado()} COP\n Total a pagar por su factura de este mes: ${totalAPagar()} COP`);
+};
 
+detalleFactura();
